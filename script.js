df55a9e5c1d95b9c0980cc96a57c9326a0a48466
happybird22 <dungeonsnotdating@gmail.com>
2025-03-17 14:31:29 -0600

    Prime Time Completed

--- script.js
+++ script.js
@@ -14,5 +14,24 @@ for (let i = 0; i <= 100; i++) {
 
 // Prime Time
 
-let n = 14
+let n = 14;
+let num = n;
+
+while (true) {
+    let prime = true;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            prime = false;
+            break;
+        }
+    }
+    if (prime && num > 1) {
+        console.log(num);
+        break;
+    }
+
+    num++;
+}
+
+// Feeling Loopy
 
